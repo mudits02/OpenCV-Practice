08a403f7afb6c71895cc027e24bb81337f86a51c
unknown <mudits02@gmail.com>
2021-06-15 22:12:18 +0530

    Update on OpenCV2

--- OpenCV2.py
+++ OpenCV2.py
@@ -5,12 +5,13 @@ import cv2
 
 cap = cv2.VideoCapture(0) # To open a video pr image , cv2.VideoCapture(filename.extension)
 print(cap.isOpened())
+print("The Width of the frame is:" , cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print("The Width of the frame is:" , cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
         cv2.imshow('frame', gray)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
